3d442eeb8fabc5ff3d71fa63ee8ff61095106705
Alua Smanova <122995436+Luakkk@users.noreply.github.com>
2024-04-03 20:26:40 +0500

    Add files via upload

---
+++ lab8/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        app_label = 'api'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'api'
